bb4cd2fdde805c2190e32e341f52cf80a4933135
dev <deveshchhabra@dev.com>
2024-06-04 18:04:18 +0530

    hsas

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -1,6 +1,6 @@
-import { Redis } from '@upstash/redis'
+// import { Redis } from '@upstash/redis'
 
-export const redis = new Redis({
-  url: 'https://diverse-bream-51740.upstash.io',
-  token:"AcocAAIncDFiMDVhMGE5NGMyMGY0YWEzYWJhMzM0ZDlmMzZkMjgxZnAxNTE3NDA",
-})
+// export const redis = new Redis({
+//   url: 'https://diverse-bream-51740.upstash.io',
+//   token:"AcocAAIncDFiMDVhMGE5NGMyMGY0YWEzYWJhMzM0ZDlmMzZkMjgxZnAxNTE3NDA",
+// })
